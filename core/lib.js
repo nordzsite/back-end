2d44e8bb0ca45000723d5903857269836c01a3cc
BuildTools <kabirdesarkar@gmail.com>
2019-10-22 23:19:08 +0530

    Setup basic environment

--- core/lib.js
+++ core/lib.js
@@ -2,6 +2,7 @@ const l = console.log;
 const path = require("path")
 
 const Lib = {
+  
   plugRouters:(routerList,app,prefix) => {
     for(let route in routerList){
       app.use(prefix+route,routerList[route])
@@ -10,9 +11,8 @@ const Lib = {
   RNG:(start,end) => {
     return start+Math.floor(Math.random()*(end-start))
   },
-  escapeRegExp:function(string) {
-  return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
-},cleanString:function(string){
+  escapeRegExp:(e) => e.replace(/[.*+?^${}()|[\]\\]/g, '\\$&') // $& means the whole matched string
+,cleanString:function(string){
     let alphabet = "abcdefghijklmnopqrstuvwxyz";
     let allowed = alphabet+alphabet.toUpperCase()+"1234567890_-";
     for(let char of string){
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const l = console.log;
+const path = require('path');
 const bodyParser = require("body-parser");
 const app = express();
 const fs = require('fs')
@@ -20,16 +21,16 @@ const connectMongo = require("connect-mongo");
 const MongoStore = connectMongo(session)
 const mongodb = require("mongodb");
 const {MongoClient} = mongodb;
-// const client = new MongoClient("mongodb://localhost")
-// const StoreClient = new MongoClient("mongodb://localhost")
-// const MongoStoreClient = new MongoStore({client:StoreClient})
-// const client = new MongoClient("mongodb://localhost",{useNewUrlParser:true})
+
 
 // Constants
 const MONGO_CONFIG = {
   useNewUrlParser:true
 }
-const KEYS = require("./keys/keys.json")
+const KEYS = require("./keys/keys.json");
+const DATA = require("./keys/data.json");
+const {MONGO_URL,MONGO_MAIN_DB} = DATA;
+l(MONGO_URL)
 const PORT = process.env.PORT || 9000;
 
 // Middleware
@@ -40,95 +41,97 @@ app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 // app.use(upload.any())
 app.use("/docs/",require("./routers/docs"));
-try {
-  app.use(session({
-    store:new MongoStore({url:"mongodb://localhost/store"}),
-    secret:KEYS.sessions,
-    resave: false,
-    saveUninitialized: false
-  }))
-} catch (e) {
-  console.log('e')
+async function main(MONGO_STORE_CLIENT=null){
+  if(MONGO_STORE_CLIENT!=null){
+    app.use(session({
+      store:new MongoStore({
+        client:MONGO_STORE_CLIENT,
+        url:MONGO_URL+MONGO_MAIN_DB
+      }),
+      secret:KEYS.sessions,
+      resave: false,
+      saveUninitialized: false
+    }))
+  } else {
+    app.use(session({
+      secret:KEYS.sessions,
+      resave: false,
+      saveUninitialized: false
+    }))
+  }
+  app.use(bodyParser.text())
+  lib.plugRouters(Routers,app,"/api/") // Plug in the routers
+  app.use("/auth",require("./routers/auth"))
+  // Routes
+  app.get("/*.html",(req,res) => {
+    let {path} = req;
+    if (fs.existsSync("public/front-end/public"+path)) {
+      fs.readFile("public/front-end/public"+path,(err,docs) => {
+        if (err) res.status(500).send("Some error occured");
+        // res.type('html').send(docs.toString())
+        docs = docs.toString();
+        if (/^((.*?)<!--(.*?)~SessionRequired~(.*?)-->)/g.test(docs) && req.session.uid == undefined) res.file("public/front-end/nosession.html")
+        else res.type('html').send(docs.toString())
+      })
+    } else {
+      res.status(404).file('public/front-end/404.html')
+    }
+  })
+  app.use(express.static("public/front-end/public"))
+  app.get("/",(req,res) => {
+    // res.send("Welcome to API")
+    // res.file("public/index.html")
+    // res.file("views/index.html")
+    res.file("public/front-end/html/assignments.html")
+  })
+  app.post("/dummy",(req,res) => {
+    res.send(req.body)
+  })
+  app.get("/*",(req,res) => {
+    // res.send(`Invalid GET route "${req.path}"`)
+    let path = req.path;
+    path = path.replace(new RegExp("%20","g")," ")
+    if(fs.existsSync("public/"+path)){
+      res.file('public/'+path)
+    } else if (path[0]=="."){
+      res.send("'.' files are forbidden")
+    } else {
+      res.status(404)
+      l(path)
+      res.file("public/front-end/404.html")
+    }
+  })
+  app.post("/*",(req,res) => {
+    res.send(`Invalid POST route "${req.path}"`)
+  })
+
+  app.listen(PORT,() => {
+    l("Server (hopefully) running on "+PORT)
+  })
 }
-app.use(bodyParser.text())
-lib.plugRouters(Routers,app,"/api/") // Plug in the routers
-app.use("/auth",require("./routers/auth"))
 
-
-
-
-// Routes
-app.get("/",(req,res) => {
-  // res.send("Welcome to API")
-  // res.file("public/index.html")
-  res.file("views/index.html")
-})
-app.get("/session",(req,res) => {
-  if (req.session.user == undefined) {
-    res.redirect("/login")
+MongoClient.connect(MONGO_URL,(err,client) => {
+  let ERR_STRING = `
+=================================================
+Running server without MongoDB connection
+=================================================
+  `;
+  if (err) {
+    console.log("Error occured in mongoDB connection");
+    main().then(() => {
+      console.log(ERR_STRING)
+    })
   } else {
-    res.file("views/session.html")
+    main(client).then(() => {
+      console.log("MongoDB Store connection success")
+    }).catch((err) => {
+      if (err.name == "MongoError" || err.name == "MongoNetworkError") {
+        main().then(() => {
+          console.log(ERR_STRING);
+        })
+      } else {
+        throw err
+      }
+    })
   }
-})
-app.get("/dummy",(req,res) => {
-  res.file("views/dummy.html");
-})
-app.post("/dummy",(req,res) => {
-  res.send(req.body)
-})
-app.get("/users",(req,res) => {
-  res.file("views/users.html")
-})
-app.get("/change/*",(req,res,next) => {
-  if(req.session.user == undefined){
-    res.redirect('/')
-  } else {
-    next()
-  }
-})
-app.get("/change/credentials",(req,res) => {
-  // res.send("Authorised")
-  res.file("views/credentials.html");
-})
-// app.get("/test",async (req,res) => {
-//   // req.session.main = "test123"
-//   try {
-//     let connection = await client.connect();
-//     let collection = connection.db("main").collection('test');
-//     let data = await collection.find().toDocs()
-//     res.send(data);
-//     client.close()
-//   } catch (e) {
-//     res.send(e)
-//     l('kjlsfd');
-//     throw e
-//   }
-// })
-app.get("/test",(req,res) => {
-  res.send("Hello world")
-})
-app.get("/login",(req,res) => {
-  res.file("views/login.html")
-})
-
-app.get("/*",(req,res) => {
-  // res.send(`Invalid GET route "${req.path}"`)
-  let path = req.path;
-  path = path.replace(new RegExp("%20","g")," ")
-  if(fs.existsSync("public/"+path)){
-    res.file('public/'+path)
-  } else if (path[0]=="."){
-    res.send("'.' files are forbidden")
-  } else {
-    res.status(404)
-    l(path)
-    res.file("html/404.html")
-  }
-})
-app.post("/*",(req,res) => {
-  res.send(`Invalid POST route "${req.path}"`)
-})
-
-app.listen(PORT,() => {
-  l("Server (hopefully) running on "+PORT)
 })
